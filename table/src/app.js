a523f0b530296df2c9049fa25f57939bea5fff24
tural.shkrov <tural.shkrov@gmail.com>
2024-01-03 14:23:10 +0400

    fix: table

--- table/src/app.js
+++ table/src/app.js
@@ -1,19 +1,19 @@
 import data from "./data.json" assert { type: 'json' };
 
 const tableHead = [
-  "Rank", "Player", "rating"
+  "Rank", "Player", "Rating"
 ]
 
 const root = document.getElementById("root");
 
 const table = document.createElement('table');
-table.classList.add('table');
+table.className = 'table table-striped';
 
 const thead = document.createElement('thead');
 const theaddRow = document.createElement('tr');
 tableHead.map(head => {
   const th = document.createElement('th');
-  th.setAttribute("scope", "col")
+  th.setAttribute("scope", "col");
   th.innerText = head;
   theaddRow.appendChild(th);
 });
@@ -29,11 +29,15 @@ for (const player of data) {
   const playerImg = document.createElement('img');
   const playerFlag = document.createElement('img');
 
+  rankTd.className = 'text-center';
+  rankTd.setAttribute("width", "80px");
   playerImg.setAttribute("src", player.img);
   playerFlag.setAttribute("src", player.flag);
   playerFlag.setAttribute("width", "15px");
   playerImg.className = 'me-3';
   playerFlag.className = 'ms-2';
+  ratingTd.setAttribute("width", "100px");
+  tr.classList.add("p-2")
 
   rankTd.innerText = "#" + player.rank;
   playerTd.appendChild(playerImg);
@@ -43,8 +47,6 @@ for (const player of data) {
   
   tr.append(rankTd, playerTd, ratingTd);
   tbody.appendChild(tr);
-  console.log(tr);
-  
 }
 
 thead.appendChild(theaddRow);
