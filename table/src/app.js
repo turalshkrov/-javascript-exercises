f9e9596b78a25e9451085ae563b8ca5670975a3c
tural.shkrov <tural.shkrov@gmail.com>
2024-01-08 12:01:50 +0400

    fix: table

--- table/src/app.js
+++ table/src/app.js
@@ -1,57 +1,31 @@
 import data from "./data.json" assert { type: 'json' };
 
-const tableHead = [
-  "Rank", "Player", "Rating"
-]
-
-const root = document.getElementById("root");
-
+const root = document.getElementsByTagName("root")[0];
+const tableHead = Object.keys(data[0]);
 const table = document.createElement('table');
 table.className = 'table table-striped';
 
 const thead = document.createElement('thead');
-const theaddRow = document.createElement('tr');
+const theadRow = document.createElement('tr');
 tableHead.map(head => {
   const th = document.createElement('th');
   th.setAttribute("scope", "col");
   th.innerText = head;
-  theaddRow.appendChild(th);
+  theadRow.appendChild(th);
 });
 
-thead.appendChild(theaddRow);
-
 const tbody = document.createElement('tbody');
-for (const player of data) {
+for (const person of data) {
   const tr = document.createElement('tr');
-  const rankTd = document.createElement('td');
-  const playerTd = document.createElement('td');
-  const ratingTd = document.createElement('td');
-  const playerImg = document.createElement('img');
-  const playerFlag = document.createElement('img');
-
-  rankTd.className = 'text-center';
-  rankTd.setAttribute("width", "80px");
-  playerImg.setAttribute("src", player.img);
-  playerFlag.setAttribute("src", player.flag);
-  playerFlag.setAttribute("width", "15px");
-  playerImg.className = 'me-3';
-  playerFlag.className = 'ms-2';
-  ratingTd.setAttribute("width", "100px");
-  tr.classList.add("p-2")
-
-  rankTd.innerText = "#" + player.rank;
-  playerTd.appendChild(playerImg);
-  playerTd.innerHTML += player.player;
-  playerTd.appendChild(playerFlag);
-  ratingTd.innerText = player.rating;
-  
-  tr.append(rankTd, playerTd, ratingTd);
+  for (const key in person) {
+    const td = document.createElement('td');
+    td.innerText = person[key];
+    tr.append(td);
+  }
   tbody.appendChild(tr);
 }
 
-thead.appendChild(theaddRow);
-
+thead.appendChild(theadRow);
 table.appendChild(thead);
 table.appendChild(tbody);
-
 root.appendChild(table);
